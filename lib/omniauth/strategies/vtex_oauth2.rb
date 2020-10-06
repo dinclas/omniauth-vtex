b85a7c7b133c40576a9d4db1bbd9a0c96c08a798
Claudio Ramos <claudio.ramos@hey.com>
2020-10-06 14:29:23 -0300

    chore: fix rubocop

--- lib/omniauth/strategies/vtex_oauth2.rb
+++ lib/omniauth/strategies/vtex_oauth2.rb
@@ -1,31 +1,31 @@
 # frozen_string_literal: true
 
-require 'omniauth-oauth2'
+require "omniauth-oauth2"
 
 module OmniAuth
   module Strategies
+    # Strategy implementation for VTEX OAuth2
     class VtexOauth2 < OmniAuth::Strategies::OAuth2
-      option :name, 'vtex_oauth2'
+      option :name, "vtex_oauth2"
       option :account
-      option :client_options, authorization_url: '/_v/oauth2/auth',
-                              token_url: '/_v/oauth2/token'
+      option :client_options, authorization_url: "/_v/oauth2/auth",
+                              token_url: "/_v/oauth2/token"
 
       option :setup, (lambda do |env|
-        strategy = env['omniauth.strategy']
+        strategy = env["omniauth.strategy"]
         account = strategy.options[:account]
 
-        env['omniauth.strategy'].options[:client_options]['site'] = "https://#{account}.myvtex.com"
+        env["omniauth.strategy"].options[:client_options]["site"] = "https://#{account}.myvtex.com"
       end)
 
-      uid { access_token['user_id'] }
+      uid { access_token["user_id"] }
 
       info do
         {
-          name: access_token['unique_name'],
-          email: access_token['email']
+          name: access_token["unique_name"],
+          email: access_token["email"]
         }
       end
-      
     end
   end
 end
--- lib/omniauth/vtex_oauth2.rb
+++ lib/omniauth/vtex_oauth2.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-require 'omniauth/strategies/vtex_oauth2'
+require "omniauth/strategies/vtex_oauth2"
